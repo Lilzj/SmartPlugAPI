dd40dda08c183872a71b26280672b080c8975234
Gbogodor Joshua <joshuagbogodor@gmail.com>
2022-02-22 14:37:10 +0100

    added service and controller

---
+++ SmartPlugAPI/Controllers/FormController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartPlugAPI.Dto.Request;
+using SmartPlugAPI.Dto.Response;
+using SmartPlugAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlugAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        private readonly FormService _service;
+
+        public FormController(FormService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("send-message/{companyName}")]
+        public async Task<ActionResult<object>> SendMessage([FromForm] FormRequestDto model, [FromRoute] string website)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Not found", "");
+                return BadRequest(ModelState);
+            }
+            //Send the message through injected 
+            var response = await _service.SendMessage(model, website);
+            if (!response.Item1) return BadRequest(new BaseResponseDto { Success = response.Item1, Message = response.Item2 });
+            return Ok(new BaseResponseDto { Success = response.Item1, Message = response.Item2 });
+        }
+    }
+}
---
+++ SmartPlugAPI/Service/FormService.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Driver;
+using SmartPlugAPI.Dto.Request;
+using SmartPlugAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlugAPI.Service
+{
+    public class FormService
+    {
+        private readonly IMongoClient _Client;
+        public FormService(IMongoClient mongoClient)
+        {
+            _Client = mongoClient;
+        }
+        public async Task<Tuple<bool, string>> SendMessage(FormRequestDto model, string website)
+        {
+            //define the database using the company name as DBName
+            var database = _Client.GetDatabase(website);
+            //Get the collection 
+            var collection = database.GetCollection<Form>("formMessages");
+            //If it is null, create new database
+            if (collection == null) await database.CreateCollectionAsync("formMessages");
+
+            //map the MessageToSendDto to Message Model
+            var form = new Form
+            {
+                CustomerName = model.CustomerName,
+                CustomerEmail = model.CustomerEmail,
+                CustomerFeedback = model.CustomerFeedback,
+                CollectionName = model.CollectionName,
+                DatabaseName = model.DatabaseName
+            };
+            try
+            {
+                //Insert the message to collection
+                await collection.InsertOneAsync(form);
+                return new Tuple<bool, string>(true, "Message Added Successfully");
+            }
+            catch (NullReferenceException e)
+            {
+                return new Tuple<bool, string>(false, "Failed to Add Message: " + e.Message.ToString());
+            }
+        }
+    }
+}
+
