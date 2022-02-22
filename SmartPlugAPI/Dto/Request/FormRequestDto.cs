71b081b4ae56f2b193df442b6488cb4a02c72833
Gbogodor Joshua <joshuagbogodor@gmail.com>
2022-02-22 14:22:38 +0100

    initial commit

---
+++ SmartPlugAPI/Dto/Request/FormRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlugAPI.Dto.Request
+{
+    public class FormRequestDto
+    {
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string CustomerEmail { get; set; }
+        [Required]
+        public string CustomerFeedback { get; init; }
+        public string CollectionName { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ SmartPlugAPI/Models/Form.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPlugAPI.Models
+{
+    public class Form
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string CustomerName { get; init; }
+        public string CustomerEmail { get; init; }
+        public string CustomerFeedback { get; init; }
+        public string CollectionName { get; set; } = "sparkPlugFeedback";
+        public string DatabaseName { get; set; } = "localhost";
+        public string CreationDate { get; set; } = DateTime.Now.ToString();
+    }
+}
